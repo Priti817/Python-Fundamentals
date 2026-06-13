14e9600f5018f91fbb3614d3e9392a459a805893
Priti Kumari <pk9082024@gmail.com>
2026-06-14 00:22:36 +0530

    Fix prime number checker logic and output messages

--- 06_Prime_number_checker/Prime_number_checker.py
+++ 06_Prime_number_checker/Prime_number_checker.py
@@ -3,8 +3,7 @@
 n=int(input("enter a no.: "))
 for i in range(2,n):
     if n%i==0:
-        print(n,"Not a Prime No.")
+        print(n,"is not a Prime No.")
         break
-    else:
-        print(n,"is a Prime No.")
-        break
+else:
+    print(n,"is a Prime No.")
